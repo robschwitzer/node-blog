fd2a1b7d3626018abf1f6c50d75e65b9aefc7c18
Rob Schwitzer <deadinthewater@me.com>
2018-05-14 07:11:54 +0000

    node app

---
+++ app.js
@@ -0,0 +1,54 @@
+var app = require('express')()
+
+var posts = [
+  {
+    id: 1,
+    author: 'John',
+    title: 'Templating with EJS',
+    body: 'Blog post number 1'
+  },
+  {
+    id: 2,
+    author: 'Drake',
+    title: 'Express: Starting from the Bottom',
+    body: 'Blog post number 2'
+  },
+  {
+    id: 3,
+    author: 'Emma',
+    title: 'Streams',
+    body: 'Blog post number 3'
+  },
+  {
+    id: 4,
+    author: 'Cody',
+    title: 'Events',
+    body: 'Blog post number 4'
+  }
+]
+//set the view engne to ejs
+app.set('view engine', 'ejs')
+
+//blog home page
+app.get('/', (req, res) => {
+  res.render('home', { posts: posts})
+})
+
+//blog post
+app.get('/post/:id', (req, res) => {
+  //find the post in the 'posts' array
+  var post = postsfilter((post) => {
+    return post.id == req.params.id
+  })[0]
+
+  //render the 'postejs' template with the post content
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body
+  })
+})
+
+app.listen(8080)
+
+console.log('Listening on port 8080');
